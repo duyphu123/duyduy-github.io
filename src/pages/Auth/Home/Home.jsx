0a9911d1eb0767b0e792713176e2750ea478a65e
duyphu123 <duytp43@wru.vn>
2022-07-06 08:23:50 +0700

    first commit

--- src/pages/Auth/Home/Home.jsx
+++ src/pages/Auth/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
+
 
 export default function Home(){
     return <div>Home</div>
--- src/pages/Auth/NotFound/NotFound.jsx
+++ src/pages/Auth/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
+
 
 export default function NotFound(){
     return <div>NotFound</div>
